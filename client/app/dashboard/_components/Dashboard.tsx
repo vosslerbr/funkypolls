95a227e0ee2e78cadbd5e2967229fad5633e9718
Brady Vossler <bradyvossler@gmail.com>
2024-01-20 09:53:16 -0600

    dashboard card updates

--- client/app/dashboard/_components/Dashboard.tsx
+++ client/app/dashboard/_components/Dashboard.tsx
@@ -6,6 +6,7 @@ import { getUserPolls } from "@/lib/actions";
 import { PollWithLinks } from "@/lib/types";
 import { useUser } from "@clerk/nextjs";
 import dayjs from "dayjs";
+import { CalendarClock, Check, Hash, X } from "lucide-react";
 import { useEffect, useState } from "react";
 import { columns } from "./Columns";
 import { DataTable } from "./DataTable";
@@ -36,37 +37,91 @@ export default function Dashboard() {
     fetchPolls();
   }, [user]);
 
+  function countOpenPolls() {
+    return userPolls.filter(({ poll }) => {
+      return poll.status === "OPEN" && dayjs(poll.expirationDate).isAfter(dayjs());
+    }).length;
+  }
+
+  function countClosedPolls() {
+    return userPolls.filter(({ poll }) => {
+      return poll.status === "CLOSED";
+    }).length;
+  }
+
+  function countExpiredPolls() {
+    // once a poll is opened, it can't be closed and that is when expiration date is set too
+    return userPolls.filter(({ poll }) => {
+      return poll.status === "OPEN" && dayjs(poll.expirationDate).isBefore(dayjs());
+    }).length;
+  }
+
   if (loading || !isLoaded || !userPolls) {
     return <Loading />;
   }
 
   return (
-    <>
-      <div className="grid grid-cols-12 gap-4 my-8">
-        <Card className="md:col-span-6 col-span-12">
-          <CardHeader>
-            <CardTitle>Total FunkyPolls</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p>{userPolls?.length || "None, create a FunkyPoll to get started!"}</p>
-          </CardContent>
-        </Card>
+    <Card className="mt-8">
+      <CardHeader>
+        <h2 className="text-2xl font-bold">My FunkyPolls</h2>
+      </CardHeader>
+      <CardContent>
+        <div className="grid grid-cols-12 gap-4 mb-8">
+          <Card className="col-span-12 sm:col-span-6 lg:col-span-3">
+            <CardHeader className="text-blue-600 bg-blue-100 mb-6 rounded-t border">
+              <CardTitle>
+                <div className="flex flex-row justify-between">
+                  Total <Hash className="w-6 h-6" />
+                </div>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>{userPolls?.length || "None, create a FunkyPoll to get started!"}</p>
+            </CardContent>
+          </Card>
 
-        <Card className="md:col-span-6 col-span-12">
-          <CardHeader>
-            <CardTitle>Active FunkyPolls</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p>
-              {userPolls?.filter(({ poll }) => {
-                return dayjs(poll.expirationDate).isAfter(dayjs());
-              })?.length || "0"}
-            </p>
-          </CardContent>
-        </Card>
-      </div>
+          <Card className="col-span-12 sm:col-span-6 lg:col-span-3">
+            <CardHeader className="text-green-600 bg-green-100 mb-6 rounded-t">
+              <CardTitle>
+                <div className="flex flex-row justify-between">
+                  Open <Check className="w-6 h-6" />
+                </div>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>{countOpenPolls()}</p>
+            </CardContent>
+          </Card>
 
-      <DataTable columns={columns} data={userPolls} />
-    </>
+          <Card className="col-span-12 sm:col-span-6 lg:col-span-3">
+            <CardHeader className="text-red-600 bg-red-100 mb-6 rounded-t">
+              <CardTitle>
+                <div className="flex flex-row justify-between">
+                  Closed <X className="w-6 h-6" />
+                </div>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>{countClosedPolls()}</p>
+            </CardContent>
+          </Card>
+
+          <Card className="col-span-12 sm:col-span-6 lg:col-span-3">
+            <CardHeader className="text-orange-600 bg-orange-100 mb-6 rounded-t">
+              <CardTitle>
+                <div className="flex flex-row justify-between">
+                  Expired <CalendarClock className="w-6 h-6" />
+                </div>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>{countExpiredPolls()}</p>
+            </CardContent>
+          </Card>
+        </div>
+
+        <DataTable columns={columns} data={userPolls} />
+      </CardContent>
+    </Card>
   );
 }
--- client/app/dashboard/_components/DataTable.tsx
+++ client/app/dashboard/_components/DataTable.tsx
@@ -1,20 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  getPaginationRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { ColumnDef, flexRender, getCoreRowModel, getPaginationRowModel, useReactTable } from "@tanstack/react-table";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -30,67 +16,50 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
   });
 
   return (
-    <Card>
-      <CardHeader>
-        <h2 className="text-2xl font-bold">Your FunkyPolls</h2>
-      </CardHeader>
-      <CardContent>
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              {table.getHeaderGroups().map((headerGroup) => (
-                <TableRow key={headerGroup.id}>
-                  {headerGroup.headers.map((header) => {
-                    return (
-                      <TableHead key={header.id}>
-                        {header.isPlaceholder
-                          ? null
-                          : flexRender(header.column.columnDef.header, header.getContext())}
-                      </TableHead>
-                    );
-                  })}
+    <>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                  ))}
                 </TableRow>
-              ))}
-            </TableHeader>
-            <TableBody>
-              {table.getRowModel().rows?.length ? (
-                table.getRowModel().rows.map((row) => (
-                  <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
-                    {row.getVisibleCells().map((cell) => (
-                      <TableCell key={cell.id}>
-                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                ))
-              ) : (
-                <TableRow>
-                  <TableCell colSpan={columns.length} className="h-24 text-center">
-                    No results.
-                  </TableCell>
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </div>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
 
-        <div className="flex items-center justify-end space-x-2 py-4">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}>
-            Previous
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}>
-            Next
-          </Button>
-        </div>
-      </CardContent>
-    </Card>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <Button variant="outline" size="sm" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+          Previous
+        </Button>
+        <Button variant="outline" size="sm" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+          Next
+        </Button>
+      </div>
+    </>
   );
 }
